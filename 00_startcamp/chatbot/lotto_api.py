7375dc04f72136aea267c2435ccdb0ab121765ab
yeseul0722 <pdy051257@naver.com>
2023-01-13 15:52:15 +0900

    middle

---
+++ 00_startcamp/chatbot/lotto_api.py
@@ -0,0 +1,60 @@
+# 파이썬으로 웹 요청 보낼 수 있는 라이브러리 불러오기
+
+# 동행복권 로또 당첨 번호 api 사용하기
+# (회차 직접 입력)
+# 입력받은 회차에 해당하는 당첨번호 확인하기 -> 6개 (보너스번호 제외)
+
+# (선택사항) - 보너스 번호 확인하기
+
+import requests
+import random
+
+# turn = input('원하는 회차를 입력해주세요')
+# response = requests.get(f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={turn}').json()
+numbers = list(range(1,7))
+
+# for no in numbers:
+#     print(response[f'drwtNo{no}'])
+
+# print(response[f'drwtNo1'])
+
+
+# 4. 이걸 1000번 반복한다.
+# 1. 로또 번호 6개를 추첨받는다.
+# 2. 내가 추첨 받은 6개의 번호가 1049회차 당첨 번호와 일치 하는지 확인한다.
+    # 2-1. 확인하는 방법은 내 번호 6개를 순회하면서
+        # 1049회 당첨번호 목록에 해당 숫자가 있는지
+        # 있다면 적중 횟수 + 1
+# 그래서 적중 횟수가 6개면 1등
+    # 5개면 3등
+    # 4개면 4등
+    # 3개면 5등
+    # 2개 이하면 꽝을 출력한다.
+
+lotto_num = list(range(1, 46))
+
+lotto_1049 = requests.get(f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1049').json()
+origin = []
+for no in numbers:
+    origin.append(lotto_1049[f'drwtNo{no}'])
+
+for i in range(1000):
+    lotto = random.sample(lotto_num, 6)
+    cnt = 0
+    for lot in lotto:
+        if lot in origin:
+            cnt += 1
+    if cnt == 6:
+        print("!!1등!!")
+    elif cnt == 5:
+        print("!2등!") 
+    elif cnt == 4:
+        print("3등") 
+    elif cnt == 3:
+        print("4등") 
+    else:
+        print("꽝") 
+               
+
+    
+
--- 00_startcamp/chatbot/random_age.py
+++ 00_startcamp/chatbot/random_age.py
@@ -1,4 +1,12 @@
 import requests
 
-r = requests.get('https://api.agify.io/?name=viktor')
+# 이름을 입력해 주세요 :
+name = input('이름을 입력해 주세요 :')
+print(name)
+
+# f -string
+# f'문자열 {변수}' /f는 format의 약어
+r = requests.get(f'https://api.agify.io/?name={name}').json()
 print(r)
+print(type(r))
+print(r['name'],  '의 나이는', r['age'], '입니다')
